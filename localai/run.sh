c7d97dd1a8451195d9da7a0fa3aba12b8e63bfa5
Yingting Huang <huangyingting@outlook.com>
2023-08-24 10:19:48 +0800

    Add localai

---
+++ localai/run.sh
@@ -0,0 +1,1 @@
+docker run -it --rm --gpus all -v ~/models:/models -p 8192:8080 huangyingting/localai
--- vllm/run.sh
+++ vllm/run.sh
@@ -1,1 +1,1 @@
-docker run --gpus all -it --rm --shm-size=8g -e MODEL=lmsys/vicuna-13b-v1.5 -v ~/models:/data -p 9090:8000 huangyingting/vllm
+docker run --gpus all -it --rm --shm-size=8g -e MODEL=lmsys/vicuna-13b-v1.5 -v ~/models:/models -p 8192:8000 huangyingting/vllm
