bd66c6d2391a3cbf4b6836d426ea97fb66d44cc4
Yingting Huang <huangyingting@outlook.com>
2023-08-24 15:48:39 +0800

    Update vllm dockerfile

--- vllm/run.sh
+++ vllm/run.sh
@@ -1,6 +1,6 @@
 docker run --gpus all -it --rm --shm-size=8g -e MODEL=facebook/opt-125m -v ~/models:/models -p 8192:8000 huangyingting/vllm
 
-curl http://localhost:9090/v1/completions \
+curl http://localhost:8192/v1/completions \
     -H "Content-Type: application/json" \
     -d '{
         "model": "facebook/opt-125m",
@@ -8,3 +8,9 @@ curl http://localhost:9090/v1/completions \
         "max_tokens": 1024,
         "temperature": 0.7
     }'
+
+curl http://localhost:8192/v1/chat/completions -H "Content-Type: application/json" -d '{
+     "model": "facebook/opt-125m",
+     "messages": [{"role": "user", "content": "What is large language model?"}],
+     "temperature": 0.9 
+   }'
