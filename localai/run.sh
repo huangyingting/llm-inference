e0752a61a4a0e9cb901eef636a3f5de1b1a39731
Yingting Huang <huangyingting@outlook.com>
2023-08-26 07:16:23 +0800

    Update tgi dockerfile

--- localai/run.sh
+++ localai/run.sh
@@ -1,7 +1,14 @@
-docker run -it --rm --gpus all -v ~/models:/models -p 8192:8080 -e PRELOAD_MODELS='[{"url":"github:huangyingting/llm-inference/localai/orca_mini_v3_7b.yaml"}]' huangyingting/localai
+#!/bin/bash
+docker run -d -it --rm --gpus all -v ~/models:/models -p 8192:8080 -e PRELOAD_MODELS='[{"url":"github:huangyingting/llm-inference/localai/orca_mini_v3_7b.yaml"}]' huangyingting/localai
+
+SECONDS=0
 
 curl http://localhost:8192/v1/chat/completions -H "Content-Type: application/json" -d '{
      "model": "orca_mini_v3_7b",
      "messages": [{"role": "user", "content": "What is large language model?"}],
      "temperature": 0.9 
    }'
+
+duration=$SECONDS
+echo ""
+echo "$duration seconds elapsed."
--- vllm/run.sh
+++ vllm/run.sh
@@ -1,4 +1,8 @@
-docker run --gpus all -it --rm --shm-size=8g -e MODEL=lmsys/vicuna-7b-v1.5 -v ~/models:/models -p 8192:8000 huangyingting/vllm
+#!/bin/bash
+
+docker run -d --gpus all -it --rm --shm-size=8g -e MODEL=lmsys/vicuna-7b-v1.5 -v ~/models:/models -p 8192:8000 huangyingting/vllm
+
+SECONDS=0
 
 curl http://localhost:8192/v1/chat/completions -H "Content-Type: application/json" -d '{
      "model": "lmsys/vicuna-7b-v1.5",
@@ -6,3 +10,7 @@ curl http://localhost:8192/v1/chat/completions -H "Content-Type: application/jso
      "messages": [{"role": "user", "content": "What is large language model?"}],
      "temperature": 0.9 
    }'
+
+duration=$SECONDS
+echo ""
+echo "$duration seconds elapsed."
