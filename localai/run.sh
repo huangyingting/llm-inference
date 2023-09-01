d49fbb37507d8641c4f2d1f2031349a1b10f0fc5
Yingting Huang <huangyingting@outlook.com>
2023-09-01 13:08:30 +0800

    Update dockerfile

--- localai/run.sh
+++ localai/run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+# LLAMA
 docker run -d -it --rm --gpus all -v ~/models:/models -p 8192:8080 -e PRELOAD_MODELS='[{"url":"github:huangyingting/llm-inference/localai/vicuna-7b-v1.5.yaml"}]' huangyingting/localai
 
 
@@ -12,3 +14,10 @@ curl http://localhost:8192/v1/chat/completions -H "Content-Type: application/jso
 duration=$SECONDS
 echo ""
 echo "$duration seconds elapsed."
+
+# Whisper
+docker run -d -it --rm --gpus all -v ~/models:/models -p 8192:8080 -e PRELOAD_MODELS='[{"url":"github:huangyingting/llm-inference/localai/whisper.yaml"}]' huangyingting/localai
+
+wget --quiet --show-progress -O gb.ogg https://upload.wikimedia.org/wikipedia/commons/1/1f/George_W_Bush_Columbia_FINAL.ogg
+
+curl http://localhost:8192/v1/audio/transcriptions -H "Content-Type: multipart/form-data" -F file="@$PWD/gb.ogg" -F model="whisper"    
